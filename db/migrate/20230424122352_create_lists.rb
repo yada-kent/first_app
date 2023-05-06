923fa22b446c1b8c857622ed8c5cac771f4c96c9
yada-kent <42689607+yada-kent@users.noreply.github.com>
2023-05-06 16:13:22 +0000

    first commit

---
+++ db/migrate/20230424122352_create_lists.rb
@@ -0,0 +1,11 @@
+class CreateLists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :lists do |t|
+
+      t.string :title
+      t.string :body
+      t.string :author
+      t.timestamps
+    end
+  end
+end
